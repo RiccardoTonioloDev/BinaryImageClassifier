a7bf79d327136fd1bdf0e29eef634a4c7991640d
Riccardo <ssctonioloriccardo@gmail.com>
2025-04-28 11:24:04 +0200

    Resizing like in resnet

--- src/BinaryImageClassifier/data.py
+++ src/BinaryImageClassifier/data.py
@@ -133,7 +133,8 @@ class FitDataManager(L.LightningDataModule):
             transform=t.Compose(
                 [
                     t.ToDtype(torch.float32, scale=False),
-                    t.Resize((300, 300)),
+                    t.Resize(300),
+                    t.CenterCrop(268),
                 ]
             ),
         )
